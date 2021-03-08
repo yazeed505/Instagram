221cfc3b778c8640f186ece663faa683c3cf7200
yazeed505 <54512668+yazeed505@users.noreply.github.com>
2021-03-07 23:54:01 -0600

    adding the rest for the second part of the insta

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -22,7 +22,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
-            
         return true
     }
 
